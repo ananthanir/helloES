846f0a3a1c16b26cae246d91074fc49ebd742d1e
Ananthan <ananthan.rajasekharan@gmail.com>
2023-02-09 00:13:34 +0530

    template

---
+++ main.js
@@ -0,0 +1,16 @@
+import express from 'express';
+import path from 'path';
+
+const server = express();
+const PORT = 3000;
+
+server.set('views', './views');
+server.set('view engine', 'ejs');
+
+server.get('/', (req, res) => {
+    res.render('index', { title: 'Express' })
+})
+
+server.listen(PORT, () => {
+    console.log(`Server started at - http://127.0.0.1:${PORT}`);
+})
